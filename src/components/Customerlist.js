14c7c5ccef424f7cf40820ee0ef92091fd319c25
ArttuOjanen <arttu.ojanen1@gmail.com>
2021-12-03 13:27:57 +0200

    Added crud

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -2,13 +2,23 @@ import React, { useState, useEffect } from 'react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import { AgGridReact } from 'ag-grid-react/lib/agGridReact';
+import Snackbar from '@mui/material/Snackbar';
+import Button from '@mui/material/Button';
+import AddCustomer from './AddCustomer';
+import EditCustomer from './EditCustomer';
+import AddTraining from './AddTraining';
 
 function Customerlist() {
 
     const [customers, setCustomers] = useState([]);
+    const [open, setOpen] = React.useState(false);
+    const [msg, setMsg] = useState('');
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     useEffect(() => {
-        // Fetch customers
        fetchCustomers();
     }, []);
 
@@ -19,6 +29,60 @@ function Customerlist() {
         .catch(err => console.error(err))
     }
 
+    const addCustomer = newCustomer => {
+        fetch('https://customerrest.herokuapp.com/api/customers', 
+        {
+            method: 'POST',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(newCustomer)
+        }
+        )
+        .then(_ => fetchCustomers())
+        .catch(err => console.error(err))
+    }
+
+    const deleteCustomer = url => {
+        if (window.confirm('Are you sure?')) {
+       fetch(url, { method: 'DELETE' })
+       .then(response => {
+        if (response.ok) {
+            fetchCustomers();
+            setMsg("Customer deleted");
+            setOpen(true);
+        }
+         else
+         alert('Poisto ei onnistunut')
+       })
+       .catch(err => console.error(err))
+     }
+    }
+
+    const editCustomer = (url, updatedCustomer) => {
+        fetch(url, {
+            method: 'PUT',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(updatedCustomer)
+        })
+        .then(_ => {
+            fetchCustomers();
+            setMsg("Customer updated");
+            setOpen(true);
+        })
+        .catch(err => console.error(err))
+    }
+
+    const addTraining = (training) => {
+        fetch('https://customerrest.herokuapp.com/api/trainings', 
+        {
+            method: 'POST',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(training)
+        }
+        )
+        .then(_ => fetchCustomers())
+        .catch(err => console.error(err))
+    }
+
     const columns = [
         {field: 'firstname', sortable: true, filter: true},
         {field: 'lastname', sortable: true, filter: true},
@@ -26,10 +90,34 @@ function Customerlist() {
         {field: 'postcode', sortable: true, filter: true, width: 120},
         {field: 'city', sortable: true, filter: true, width: 120},
         {field: 'email', sortable: true, filter: true, width: 120},
-        {field: 'phone', sortable: true, filter: true, width: 120}
+        {field: 'phone', sortable: true, filter: true, width: 120},
+        {
+            headerName: '',
+            width: 170,
+            cellRendererFramework: params => <AddTraining addTraining={addTraining} customer={params.data} />
+        },
+        {
+            headerName: '',
+            sortable: false,
+            filter: false,
+            width: 120,
+            field: 'links.0.href',
+            cellRendererFramework: params => <EditCustomer editCustomer={editCustomer} customer={params} />
+        },
+        {
+            headerName: '',
+            sortable: false,
+            filter: false,
+            width: 120,
+            field: 'links.0.href',
+            cellRendererFramework: params => <Button size="small" color="error" onClick={() => deleteCustomer(params.value)}>Delete</Button>   
+        }
+        
     ]
 
     return(
+        <div>
+            <AddCustomer addCustomer={addCustomer} />
         <div className="ag-theme-material" style={{marginTop: 20, height: 600, width: '80%', margin: 'auto'}}>
             <AgGridReact
             rowData={customers}
@@ -38,6 +126,13 @@ function Customerlist() {
             paginationPageSize={10}
             suppressCellSelection={true}
             />
+        </div>
+        <Snackbar 
+          open={open}
+          message={msg}
+          autoHideDuration={3000}
+          onClose={handleClose}
+          />
         </div>
     );
 }
--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -3,29 +3,67 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import { AgGridReact } from 'ag-grid-react/lib/agGridReact';
 import dayjs from 'dayjs';
+import Snackbar from '@mui/material/Snackbar';
+import Button from '@mui/material/Button';
 
 function Traininglist() {
 
-    const [training, setTraining] = useState([]); 
+    const [training, setTraining] = useState([]);
+    const [open, setOpen] = React.useState(false);
+    const [msg, setMsg] = useState(''); 
+
+    const handleClose = () => {
+        setOpen(false);
+      };
 
     useEffect(() => {
        fetchTrainings();
     }, []);
 
     const fetchTrainings = () => {
-        fetch('https://customerrest.herokuapp.com/api/trainings')
+        fetch('https://customerrest.herokuapp.com/gettrainings')
         .then(response => response.json())
-        .then(data => setTraining(data.content))
+        .then(data => setTraining(data))
         .catch(err => console.error(err))
     }
+
+    const deleteTraining = url => {
+        if (window.confirm('Are you sure?')) {
+       fetch(url, { method: 'DELETE' })
+       .then(response => {
+        if (response.ok) {
+            fetchTrainings();
+            setMsg("Training deleted");
+            setOpen(true);
+        }
+         else
+         alert('Poisto ei onnistunut')
+       })
+       .catch(err => console.error(err))
+    }
+}
 
     const columns = [
         {field: 'date', sortable: true, filter: true, valueFormatter: (params) => {return dayjs(params.value).format('DD.MM.YYYY hh:mm')}},
         {field: 'duration', sortable: true, filter: true},
-        {field: 'activity', sortable: true, filter: true}
+        {field: 'activity', sortable: true, filter: true},
+        {
+            headerName: "Customer",
+            cellRendererFramework: params => params.data.customer.firstname + " " + params.data.customer.lastname
+        },
+        {
+            headerName: '',
+            sortable: false,
+            filter: false,
+            width: 120,
+            field: 'links.0.href',
+            cellRendererFramework: params => <Button size="small" color="error" onClick={() => deleteTraining("https://customerrest.herokuapp.com/api/trainings/" + params.data.id)}>Delete</Button>
+        }
     ]
 
     return(
+        <div>
+        
         <div className="ag-theme-material" style={{marginTop: 20, height: 600, width: '50%', margin: 'auto'}}>
             <AgGridReact
             rowData={training}
@@ -33,8 +71,14 @@ function Traininglist() {
             pagination={true}
             paginationPageSize={10}
             suppressCellSelection={true}
-            columns
             />
+        </div>
+        <Snackbar 
+          open={open}
+          message={msg}
+          autoHideDuration={3000}
+          onClose={handleClose}
+          />
         </div>
     );
 }
